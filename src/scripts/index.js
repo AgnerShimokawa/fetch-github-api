7c774b9ae3f7b08c31a11b3a18e65c2799c0b481
Agner <80971302+AgnerShimokawa@users.noreply.github.com>
2024-11-11 02:52:17 +0900

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import { getUser } from "./services/user.js";
 import { getRepositories } from "./services/repositories.js";
+import { getEvents } from "./services/events.js";
 
 import { user } from "./objects/user.js"
 import { screen } from "./objects/screen.js"
@@ -9,7 +10,6 @@ document.getElementById('btn-search').addEventListener('click', () => {
     if(validateEmptyInput(userName)) return
     getUserData(userName);
 })
-
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
     const userName = e.target.value;
@@ -33,18 +33,18 @@ async function getUserData(userName){
 
     const userResponse = await getUser(userName);
 
-    console.log(userResponse);
-    
-
     if(userResponse.message === "Not Found"){
         screen.renderNotFound()
         return
     }
 
     const repositoriesResponse = await getRepositories(userName)
+
+    const eventsResponse = await getEvents(userName)
     
     user.setInfo(userResponse)
     user.setRepositories(repositoriesResponse)
+    user.setEvents(eventsResponse)
     
     screen.renderUser(user)
 }
